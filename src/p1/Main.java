63b837ae903381bc9530ef69f8bcfa5393b7ce52
carguell <carguell@dtcc.edu>
2016-02-02 13:37:31 -0500

    Sorted Hand

--- src/p1/Main.java
+++ src/p1/Main.java
@@ -1,4 +1,7 @@
 package p1;
+
+import java.lang.reflect.Array;
+
 /*
 Card Shuffle
 Demo Program
@@ -9,51 +12,82 @@ Version .01
 public class Main {
 
     public static void main(String[] args) {
-	    int[] deck = new int[52];
+        int[] deck = new int[52];
         String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-
-        //Initialize the card
-        for(int i = 0; i < deck.length; i++){
-            deck[i] = i;
-        }
-        //shuffle the cards
-        for (int i = 0; i < deck.length; i++){
-            // generate an index randomly
-            int index = (int)(Math.random() * deck.length);
-            int temp = deck[i];
-            deck[i] = deck[index];
-            deck[index] = temp;
-        }
-        //Deal hand
         int[][] playerCard =  new int [4][13];
+        boolean [][][] sortHands = new boolean [4][4][13];
 
-        for (int i = 0; i < 4; i ++){
-            for(int j = 0; j < 13; j++) {
-                if (i == 0)
-                    playerCard[i][j] = deck[j];
-                else if (i == 1)
-                    playerCard[i][j] = deck[j+13];
-                else if (i == 2)
-                    playerCard[i][j] = deck[j+26];
-                else if (i == 3)
-                    playerCard[i][j] = deck[j+39];
+        initialize(deck);
+        shuffle(deck);
+        //dealHands(playerCard, deck, suits, ranks);
+        sortHand(sortHands, deck, suits, ranks);
+    }
+
+    //Initialize the card
+    public static void initialize(int[] cards) {
+
+        for (int i = 0; i < cards.length; i++)
+            cards[i] = i;
+    }
+         //shuffle the cards
+        public static void shuffle(int[] cardShuffle) {
+
+            for (int i = 0; i < cardShuffle.length; i++) {
+                // generate an index randomly
+                int index = (int) (Math.random() * cardShuffle.length);
+                cardShuffle[i] = cardShuffle[index];
             }
         }
-        for (int i = 0; i < 4; i ++){
-            for(int j = 0; j < 13; j++) {
-                String suit = suits[playerCard[i][j] / 13];
-                String rank = ranks[playerCard[i][j] % 13];
-                if (i == 0)
-                    System.out.println("Chung's card: " + rank + " " + suit);
-                else if (i == 1)
-                    System.out.println("John's card: " + rank + " " + suit);
-                else if (i == 2)
-                    System.out.println("Erick's card: " + rank + " " + suit);
-                else if (i == 3)
-                    System.out.println("Robert's card: " + rank + " " + suit);
+
+   public static boolean sortHand(boolean[][][] sort, int[] cards, String[] cardSuit, String[] cardRank){
+            int suit;
+            int rank;
+
+       for(int i = 0 ; i < 4; i++){
+            for( int j = 0; j < 4; j++){
+                for(int h = 0; h < 13; h++){
+                    if(i == 0){
+                         suit = cards[h] / 13;
+                         rank = cards[h] % 13;
+                    }
+                    else if(i == 1){
+                        suit = cards[h + 13] / 13;
+                        rank = cards[h + 13] % 13;
+                    }
+                    else if(i == 2){
+                        suit = cards[h + 26] / 13;
+                         rank = cards[h + 26] % 13;
+                    }
+                    else{
+                        suit = cards[h + 39] / 13;
+                        rank = cards[h + 39] % 13;
+                    }
+                    sort[i][suit][rank] = true;
+                }
             }
-            System.out.println();
+           System.out.println("\n");
         }
+
+
+       for(int i = 0 ; i < 4; i++){
+           for( int j = 0; j < 4; j++){
+               for(int h = 0; h < 13; h++){
+                   if (sort[i][j][h]) {
+                       if (i == 0) {
+                           System.out.println("CHung's card " + cardRank[h] + " " + cardSuit[j]);
+                       } else if (i == 1) {
+                           System.out.println("John's card " + cardRank[h] + " " + cardSuit[j]);
+                       } else if (i == 2) {
+                           System.out.println("Erick's card " + cardRank[h] + " " + cardSuit[j]);
+                       } else if (i == 3) {
+                           System.out.println("Robert's card " + cardRank[h] + " " + cardSuit[j]);
+                       }
+                   }
+               }
+           }
+           System.out.println();
+       }
+            return true;
     }
 }
